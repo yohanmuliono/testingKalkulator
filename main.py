e3f91b122bbe3621aec19bcc37a9852061b1785b
Ardian Danny <65061019+shatternox@users.noreply.github.com>
2021-12-22 14:38:08 +0700

    update terakhir dari danny

--- main.py
+++ main.py
@@ -1,9 +1,59 @@
+
 def penambahan(a,b):
-	total = a+b
-	return total
+    return a + b
+
+
+def pengurangan(a,b):
+    return a-b
+
+
+def perkalian(a,b):
+    return a*b
+
+
+def pembagian(a,b):
+    return a/b
+
 
 def main():
-	print (penambahan(5,5))
+    print(penambahan(10,5))
+    print(pengurangan(10,5))
+    print(perkalian(10,5))
+    print(pembagian(10,5))
+    
+    
+if __name__ == "__main__":
+    main()
 
-main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
